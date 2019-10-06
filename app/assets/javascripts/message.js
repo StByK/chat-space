31eaead890aa0abc6e2e2b92b293fd1f72243f0e
StByK <miz07og18kag@gmail.com>
2019-10-06 20:53:32 +0900

    add ajax function

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,5 +1,17 @@
 $(function() {
 
+  function buildHTML(message) {
+    var image = ""
+    message.image ? image = `<img src="${message.image}">` : image = ""
+    var html = `<div class="message-sender">${message.name}</div>
+              <div class="message-date">${message.time}</div>
+              <div class="message-text">
+                <p>${message.text}</p>
+                ${image}
+              </div>`
+    return html;
+  }
+
   $(".form__box").submit(function(e) {
     e.preventDefault();
     var formData = new FormData(this);
@@ -11,6 +23,11 @@ $(function() {
       dataType: 'json',
       processData: false,
       contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.message__item').append(html)
+      $('input__box').val('')
     })
   })
 });
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -7,15 +7,19 @@ class MessagesController < ApplicationController
   end
 
   def create
-    binding.pry
-    @message = @room.messages.new(message_params)
-    if @message.save
-      redirect_to room_messages_path(@room), notice: 'メッセージが送信されました'
-    else
-      @messages = @room.messages.includes(:user)
-      flash.now[:alert] = 'メッセージを入力してください'
-      render :index
-    end 
+    @message = @room.messages.create(message_params)
+    respond_to do |format|
+      format.html {redirect_to room_messages_path(@room)}
+      format.json
+    end
+
+    # if @message.save
+    #   redirect_to room_messages_path(@room), notice: 'メッセージが送信されました'
+    # else
+    #   @messages = @room.messages.includes(:user)
+    #   flash.now[:alert] = 'メッセージを入力してください'
+    #   render :index
+    # end 
   end
 
   private
