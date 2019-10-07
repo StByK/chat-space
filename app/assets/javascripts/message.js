5f6d936bad8c11bbb56c966d635303cb6cef432d
StByK <miz07og18kag@gmail.com>
2019-10-07 11:09:53 +0900

    add auto scroll

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,21 +1,23 @@
 $(function() {
 
+  
   function buildHTML(message) {
     var image = ""
     message.image ? image = `<img src="${message.image}">` : image = ""
     var html = `<div class="message-sender">${message.name}</div>
-              <div class="message-date">${message.time}</div>
-              <div class="message-text">
-                <p>${message.text}</p>
-                ${image}
-              </div>`
+    <div class="message-date">${message.time}</div>
+    <div class="message-text">
+    <p>${message.text}</p>
+    ${image}
+    </div>`
     return html;
   }
-
-  $(".form__box").submit(function(e) {
+  
+  $('.form__box').submit(function(e) {
     e.preventDefault();
     var formData = new FormData(this);
     var url = $(this).attr('action')
+    $('.send__btn').removeAttr('data-disable-with');
     $.ajax({
       type: 'POST',
       url: url,
@@ -27,7 +29,11 @@ $(function() {
     .done(function(data){
       var html = buildHTML(data);
       $('.message__item').append(html)
-      $('input__box').val('')
+      $('.message__form').val('')
+      $('.main__messages').animate({scrollTop: $('.main__messages')[0].scrollHeight}, 'fast');
     })
+    .fail(function() {
+      alert('投稿できませんでした')
+    });
   })
 });
